cd7dfb885c9ed9d75d574ee74a2cae7da91a3342
crazyhouse33 <hugo.alonso@etu.u-bordeaux.fr>
2020-07-22 14:48:17 +0200

    adjusting distribution with pip

--- setup.py
+++ setup.py
@@ -7,18 +7,17 @@ with open("README.md") as fh:
 setuptools.setup(
     name="kvhf", 
     version=version,
-    author="Alonso Hugo",
+    author="Hugo A",
     author_email="author@example.com",
     description="Python package to manipulate kvhf files",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/crazyhouse33/kvhf.git",
     packages=setuptools.find_packages(),
-    test_suite="tests",   
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.3',
 )
---
+++ tests/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bash -c 'cd test_work_dir; PYTHONPATH=../.. python3.7 -m pytest -x ..'
